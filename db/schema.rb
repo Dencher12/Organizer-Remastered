24122c4a51e763a4e885eda122247601ba62f8fc
Dencher12 <dencher12@protonmail.com>
2020-12-14 17:25:09 +0300

    create notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_132650) do
+ActiveRecord::Schema.define(version: 2020_12_14_135817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+  create_table "notes", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "text"
+    t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
